1fde51dc1213f87585d5562475440a73d1d457ff
David Velasco Daza <david.velasco.2020@upb.edu.co>
2020-03-26 13:50:44 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 13:16:50 2020
+
+@author: david
+"""
+
+
+a = int(input("Ingrese el 1er número: "))
+c = int(input("Ingrese el 2do número: "))
+
+print("El producto de los números es: " + str(a*c))
+print("El doble del 1er número es: " + str(a*2))
