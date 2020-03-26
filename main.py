bb008df9b4257d0ee1e1bd2409a79560f81d5ef8
David Velasco Daza <david.velasco.2020@upb.edu.co>
2020-03-26 15:56:07 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -6,8 +6,37 @@ Created on Thu Mar 26 13:16:50 2020
 """
 
 
-a = int(input("Ingrese el 1er número: "))
-c = int(input("Ingrese el 2do número: "))
+a = float(input("Ingrese el 1er número: "))
+c = float(input("Ingrese el 2do número: "))
 
 print("El producto de los números es: " + str(a*c))
 print("El doble del 1er número es: " + str(a*2))
+
+
+#%% Punto 10
+
+b = float(input("Ingrese un 1er número: "))
+d = float(input("Ingrese un 2do número: "))
+pot = b**2
+raiz = d**(1/2)
+
+print("El cuadrado del 1er número es: " + str(pot))
+print("La raíz del 2do número es: " + str(raiz))
+
+#%% Punto 12
+
+a = float(input("Ingrese el número a: "))
+b = float(input("Ingrese el número b: "))
+c = float(input("Ingrese el número c: "))
+d = b**2 - 4*a*c
+if d > 0:
+    x1 = (-b+d**(1/2))/(2*a)
+    x2 = (-b-d**(1/2))/(2*a)
+    print("La primera solución es: " + str(x1))
+    print("La segunda solución es: " + str(x2))
+else:
+    if d==0:
+        x = -b/2*a
+        print("x1 y x2 son iguales y corresponden a: " + str(x))
+    else:
+        print("No existe solución.")
